4765b4539ad258ff09b4c943793ba7e44c7137e4
krishna0852 <krishnatejapalla1@gmail.com>
2024-01-28 20:57:07 +0530

    shell-script to install awscli,kubectl,eksctl

---
+++ aws-cli-eksctl-kubectl.sh
@@ -0,0 +1,110 @@
+#!/bin/bash 
+getid=$(id -u)
+
+if [ $getid -ne 0 ];
+  then
+  echo "please run the script with root privileages"
+fi 
+
+awscli="https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip"
+eksctl=""https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp"
+kubectl_version=1.27.9
+kubectl="https://s3.us-west-2.amazonaws.com/amazon-eks/$kubectl_version/2024-01-04/bin/linux/amd64/kubectl"
+
+validateCmndStatus(){
+cmndsts=$1
+description=$2
+if [ $cmndsts -ne 0 ]; then 
+  echo  "$description failure"
+  exit 1
+else 
+  echo "$description success"
+fi
+
+}
+
+apt-get update -y 
+
+validateCmndStatus $? "updating packages is "
+
+echo "installing unzip"
+
+apt-get install unzip -y 
+
+validateCmndStatus $? "installing unzip is "
+
+echo "installing aws cli"
+
+curl $awscli -o "awscliv2.zip"
+
+validateCmndStatus $? "importing aws cli file using curl"
+
+unzip awscliv2.zip
+
+validateCmndStatus $? "unzipping awscli is" 
+
+echo "install in the newly unzipped aws directory. By default, the files are all installed to /usr/local/aws-cli, and a symbolic link is created in /usr/local/bin"
+
+ ./aws/install
+
+validateCmndStatus $? "creating symbolic link is "
+
+echo "awscli-installed, checking aws cli version"
+
+aws_cli_version=$(aws --version) 
+
+validateCmndStatus $? " version $aws_cli_version is "
+
+echo "awscli successfully installed, proceeding to install eksctl"
+
+curl --silent --location $eksctl
+
+validateCmndStatus $? "installing latest version of eksctl using curl is"
+
+echo "Move the extracted binary to /usr/local/bin."
+
+mv /tmp/eksctl /usr/local/bin
+
+validateCmndStatus $? "extracting to binary to bin is "
+
+echo "checking the eksctl version"
+
+eksctl_version=$(eksctl version)
+
+validateCmndStatus $? "version $eksctl_version is "
+
+echo "eksctl installed successfully, proceeding to install kubectl with $kubectl_version "
+
+curl -O $kubectl
+
+validateCmndStatus $? "installing kubectl binaries is "
+
+echo "Applying execute permissions to the binary."
+
+chmod +x ./kubectl
+
+validateCmndStatus $? "applying execute permission is "
+
+echo "Copy the binary to a folder in your PATH."
+
+mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH
+
+validateCmndStatus $? "copying binary is "
+
+echo "kubectl installed, checking kubectl version"
+
+installed_kubectl_version=$(kubectl version)
+
+validateCmndStatus $? "version $installed_kubectl_version"
+
+echo "=====AWSCLI, EKSCTL, KUBECTL INSTALLED SUCCESSFULLY=="
+
+echo "configure aws credentials and launch eks cluster"
+exit 0
+
+
+
+
+
+
+
