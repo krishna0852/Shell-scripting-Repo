170ff5a2fb53e1b1c054cab96e19b9b64ebc7308
krishna0852 <krishnatejapalla1@gmail.com>
2024-11-21 17:06:39 +0530

    Added alternate approach

--- conditionalStatements/01-checkVariableIsEmpty.sh
+++ conditionalStatements/01-checkVariableIsEmpty.sh
@@ -12,3 +12,10 @@ echo "Entered UserName: $userName"
 
 
 #In shell scripting, the -z option is used with the test command (or [ in a conditional statement) to check if a string is empty.
+
+if [ "$1" = "" ]; then    # you can also validate in this approach
+    echo "variable is empty"
+else 
+    echo "variable is not empty"
+fi 
+
